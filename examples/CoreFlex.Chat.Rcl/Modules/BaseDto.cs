e333ed86c4062b8d61e3281b314b103be3d7a471
token <239573049@qq.com>
2023-12-12 00:36:54 +0800

    优化样式显示

--- examples/CoreFlex.Chat.Rcl/Modules/BaseDto.cs
+++ examples/CoreFlex.Chat.Rcl/Modules/BaseDto.cs
@@ -8,4 +8,9 @@ public abstract class BaseDto<TKey>
     /// 创建时间
     /// </summary>
     public DateTime CreatedTime { get; set; }
+
+    /// <summary>
+    /// 点击事件
+    /// </summary>
+    public Func<Task> OnClick { get; set; }
 }
--- examples/CoreFlex.Chat.Rcl/Pages/Index.razor.cs
+++ examples/CoreFlex.Chat.Rcl/Pages/Index.razor.cs
@@ -4,6 +4,8 @@ public partial class Index
 {
     private List<ChatMenuItem> chatMenuItems = new();
 
+    private ChatMenuItem selectMenuItem;
+
     private const string ChatMenuItemKey = "ChatMenuItemKey";
 
     protected override async Task OnInitializedAsync()
@@ -14,8 +16,23 @@ public partial class Index
         }
         else
         {
-            chatMenuItems.Add(new ChatMenuItem(Guid.NewGuid(), "智能AI助手", "一个AI智能助手", "gpt-3.5-turbo-1106", string.Empty, 2000));
+            var menu = new ChatMenuItem(Guid.NewGuid(), "1智能AI助手", "一个AI智能助手", "gpt-3.5-turbo-1106", string.Empty);
+            menu.OnClick += async () => await OnSelect(menu);
+            await OnSelect(menu);
+            chatMenuItems.Add(menu);
+
+
+            var menu1 = new ChatMenuItem(Guid.NewGuid(), "2智能AI助手", "一个AI智能助手", "gpt-3.5-turbo-1106", string.Empty);
+            menu1.OnClick += async () => await OnSelect(menu1);
+            await OnSelect(menu1);
+            chatMenuItems.Add(menu1);
         }
+    }
+
+    private async Task OnSelect(ChatMenuItem item)
+    {
+        selectMenuItem = item;
+        await InvokeAsync(StateHasChanged);
     }
 
     private string GetLastUpdate(DateTime? lastUpdate)
