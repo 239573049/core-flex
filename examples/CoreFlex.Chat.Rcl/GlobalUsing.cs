37ff57e2fcbbb1cabe2f51fdb1532ef5a59a23ca
token <239573049@qq.com>
2023-12-20 21:58:48 +0800

    删除示例

--- examples/CoreFlex.Chat.Rcl/GlobalUsing.cs
+++
@@ -1,7 +0,0 @@
-﻿/// core-flex-chat © 2023-12-13 by 贺家乐 is licensed under Attribution-NonCommercial-ShareAlike 4.0 International 1
-global using BlazorComponent;
-global using Masa.Blazor;
-global using Masa.Blazor.Presets;
-global using CoreFlex.Chat.Rcl.Modules;
-global using CoreFlex.Chat.Contract;
-global using CoreFlex.Jwt;
--- examples/CoreFlex.Chat.Server/CoreFlex.Chat.Server/GlobalUsing.cs
+++
@@ -1,4 +0,0 @@
-﻿/// core-flex-chat © 2023-12-13 by 贺家乐 is licensed under Attribution-NonCommercial-ShareAlike 4.0 International 1
-global using CoreFlex.Chat.Contract;
-global using CoreFlex.Module;
-global using CoreFlex.Jwt;
